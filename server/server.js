69ad3139d0c36781b5b6e755a76dee592d0f65aa
Eric Sanchirico <eccisketchi@gmail.com>
2022-01-16 05:06:12 -0500

    deployed with HTTPS

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ const app = express();
 const abi = require('./abicode.js');
 const ethTx = require('ethereumjs-tx').Transaction
 const Common = require('ethereumjs-common').default;
+/*
+------------ADD IN ON SERVER FOR HTTPS------------
+const https = require('https');
+const fs = require('fs');
+var cert = fs.readFileSync('/etc/letsencrypt/live/ericsweb3api.com/fullchain.pem');
+var key = fs.readFileSync('/etc/letsencrypt/live/ericsweb3api.com/privkey.pem');
+var options = { key: key, cert: cert };
+
+NOTE: SSL certs must be generated first
+*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -311,11 +321,20 @@ app.post('/submitreg', (req, res) => {
     }
 })
 
-//Port number to listen on. Default is 3000
-const PORT = 3000;
+//Port number to listen on. Default is 3000. 
+const PORT = 3000; //Change to 443 for HTTPS
 
 
 //start listening
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+app.listen(PORT, () => {                      //comment out for HTTPS
+  console.log(`Server listening on ${PORT}`); //comment out for HTTPS
+});                                           //comment out for HTTPS
+
+
+
+/* ------MUST BE ADDED IN FOR HTTPS-------------
+var server = https.createServer(options, app);
+server.listen(PORT, () => {
+     console.log(`Server listening on ${PORT}`);
+ });
+*/
